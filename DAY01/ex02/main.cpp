52080c46e2822b6b5f8114b9b0d580522f18f448
ezahiri <ezahiri@DESKTOP-VCUBF20.localdomain>
2024-12-12 22:50:15 +0100

    ex02

---
+++ DAY01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/12 22:29:37 by ezahiri           #+#    #+#             */
+/*   Updated: 2024/12/12 22:47:05 by ezahiri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main ()
+{
+    std::string s = "HI THIS IS BRAIN";
+    std::string *stringPTR = &s;
+    std::string& stringREF = s;
+
+    std::cout << "• The memory address of the string variable is : " << &s << std::endl;
+    std::cout << "• The memory address held by stringPTR is : " << stringPTR << std::endl;
+    std::cout << "• The memory address held by stringREF is : " << &stringREF  << std::endl;
+    
+    std::cout << "• The value of the string variable is : " << s << std::endl;
+    std::cout << "• The value pointed to by stringPTR is : " << *stringPTR << std::endl;
+    std::cout << "• The value pointed to by stringREF is : " << stringREF << std::endl;
+     
+}
