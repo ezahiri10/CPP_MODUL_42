e33ef6d60326edb9f51012b783362132b6b3a7f0
El Mustapha Zahiri <ezahiri@e2r6p7.1337.ma>
2024-12-21 13:17:56 +0100

    ex02 is finish

--- DAY02/ex02/Fixed.cpp
+++ DAY02/ex02/Fixed.cpp
@@ -6,45 +6,37 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 15:37:59 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/12/21 12:55:01 by ezahiri          ###   ########.fr       */
+/*   Updated: 2024/12/21 13:13:24 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed () : fixedPointValue(0)
-{
-    // std::cout << "Default constructor called" << std::endl;
-}
+Fixed::Fixed () : fixedPointValue(0) {}
 
 Fixed::Fixed(const int val)
 {
-    // std::cout << "Int constructor called" << std::endl;
     fixedPointValue = val << fractionalBits;
 }
 
 Fixed::Fixed (const float val)
 {
-    // std::cout << "Float constructor called" << std::endl;
     fixedPointValue = roundf(val * (1 << fractionalBits));
 }
 
 Fixed::Fixed (const Fixed &F)
 {
-    // std::cout << "Copy constructor called" << std::endl;
     *this = F; 
 }
 
 Fixed &Fixed::operator=(const Fixed &F)
 {
-    // std::cout << "Copy assignment operator called" << std::endl;
     fixedPointValue = F.getRawBits();
     return (*this);
 }
 
 Fixed::~Fixed ()
 {
-    // std::cout << "Destructor called" << std::endl;
 }
 
 void Fixed::setRawBits (int const fixedPointValue)
