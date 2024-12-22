678484c20381e87c7fed35e86464d258a7efe8a7
El Mustapha Zahiri <ezahiri@e2r6p7.1337.ma>
2024-12-22 10:54:19 +0100

    Fixed ex03 in Day02

--- DAY02/ex03/bsp.cpp
+++ DAY02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 19:03:20 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/12/21 22:21:49 by ezahiri          ###   ########.fr       */
+/*   Updated: 2024/12/21 23:04:23 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +19,37 @@ std::ostream &operator<< (std::ostream &os, const Fixed &F)
 }
 
 
-const Fixed Area (Point const &P1, Point const &P2, Point const &P3)
+Fixed Area (Point const &P1, Point const &P2, Point const &P3)
 {
     Fixed res = ((P2.get_x () - P1.get_x ()) * (P3.get_y () - P1.get_y ()) 
         - (P3.get_x () - P1.get_x ()) * (P2.get_y () - P1.get_y()));
-    return (Fixed(0.5f) * res);
+    return (Fixed(0.5f * abs(res.toFloat())));
 }
 
 bool bsp( Point const &a, Point const &b, Point const &c, Point const &point)
 {
-    Fixed abc = Area (a, b, c);
-    Fixed abp = Area (a, b, point);
-    Fixed acp = Area (a, c, point);
-    Fixed bcp = Area (b, c, point);
-    // std::cout << "abc: " << abc << std::endl;
-    // std::cout << "abp: " << abp << std::endl;
-    // std::cout << "acp: " << acp << std::endl;
-    // std::cout << "bcp: " << bcp << std::endl;
-    return (abc == abp + acp + bcp);
+    Fixed totalArea = Area(a, b, c);
+    Fixed area1 = Area(a, b, point);
+    Fixed area2 = Area(a, point, c);
+    Fixed area3 = Area(point, b, c);
+
+    std::cout << "Total Area: " << totalArea << std::endl;
+    std::cout << "Area1: " << area1 << std::endl;
+    std::cout << "Area2: " << area2 << std::endl;
+    std::cout << "Area3: " << area3 << std::endl;
+    if (totalArea == Fixed(0) || area1 == Fixed(0) || area2 == Fixed(0) || area3 == Fixed(0))
+        return (false);
+    if (totalArea == (area1 + area2 + area3))
+        return (true);
+    return (false);
 }
 
 int main ()
 {
     Point a(Fixed(0), Fixed(0));
     Point b(Fixed(0), Fixed(4));
-    Point c(Fixed(4), Fixed(0));
-    Point point(3, 1);
+    Point c(Fixed(5), Fixed(0));
+    Point point(Fixed(0.5f), Fixed(0.3f));
 
     if (bsp(a, b, c, point))
         std::cout << "The point is inside the triangle" << std::endl;
