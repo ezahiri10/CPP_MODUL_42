2848c33e28b9412aaf9f8c186ca0df64e00ea651
ezahiri <ezahiri@DESKTOP-VCUBF20.localdomain>
2024-12-29 19:19:03 +0100

    ex00

--- DAY04/ex00/main.cpp
+++ DAY04/ex00/main.cpp
@@ -6,22 +6,37 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 17:46:07 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/12/29 18:15:16 by ezahiri          ###   ########.fr       */
+/*   Updated: 2024/12/29 19:16:56 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
 
-int main()
-{
-    Animal* meta = new Animal();
-    Animal* j = new Dog();
-    Animal* i = new Cat();
-    std::cout << j->get_type() << " " << std::endl;
-    std::cout << i->get_type() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
-    meta->makeSound ();
+int main() {
+    const Animal* meta = new Animal();
+    const Animal* dog = new Dog();
+    const Animal* cat = new Cat();
+
+    std::cout << dog->getType() << " " << std::endl;
+    std::cout << cat->getType() << " " << std::endl;
+
+    cat->makeSound();  
+    dog->makeSound();  
+    meta->makeSound(); 
+    const WrongAnimal* wrongMeta = new WrongAnimal();
+    const WrongAnimal* wrongCat = new WrongCat();
+
+    wrongCat->makeSound();
+    wrongMeta->makeSound();
+
+    delete meta;
+    delete dog;
+    delete cat;
+
+    delete wrongMeta;
+    delete wrongCat;
+
     return 0;
 }
+
