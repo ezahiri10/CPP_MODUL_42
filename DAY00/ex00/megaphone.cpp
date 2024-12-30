4cb4dee350d6f6bfdb1326e470379ca47c25aeb3
El Mustapha Zahiri <ezahiri@e2r4p7.1337.ma>
2024-12-30 20:01:48 +0100

    fix some probelem in ex00 in DAY00

--- DAY00/ex00/megaphone.cpp
+++ DAY00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 15:41:02 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/12/15 12:20:01 by ezahiri          ###   ########.fr       */
+/*   Updated: 2024/12/30 20:00:02 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void print_upper (std::string s)
 	for (int i = 0; s[i] != '\0' ; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
-			std::cout << (char)(s[i] - 32);
+			std::cout << (char)std::toupper(s[i]);
 		else
 			std::cout << s[i];
 	}
