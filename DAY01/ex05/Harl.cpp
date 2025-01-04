b65f2adedec206fecb291696d44456d28e511b96
El Mustapha Zahiri <ezahiri@e2r5p7.1337.ma>
2025-01-04 15:33:07 +0100

    fix some problem in DAY01

--- DAY01/ex05/Harl.cpp
+++ DAY01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 14:33:28 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/12/15 15:01:22 by ezahiri          ###   ########.fr       */
+/*   Updated: 2025/01/04 15:31:13 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,25 @@
 
 void Harl::debug ( void )
 {
+    std::cout << "[ ERROR ] ";
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl; 
 }
 
 void Harl::info ( void )
 {
+    std::cout << "[ INFO ] ";
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"  << std::endl;
 }
 
 void Harl::warning ( void )
 {
+    std::cout << "[ WARNING ] ";
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error ( void )
 {
+    std::cout << "[ INFO ] ";
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
