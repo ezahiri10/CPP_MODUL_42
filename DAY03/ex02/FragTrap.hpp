1cd2a7c70216a4695b1ce0f3fa9478bfed5ae851
El Mustapha Zahiri <ezahiri@e2r6p3.1337.ma>
2025-01-07 23:31:45 +0100

    4form in FRAGtrap

--- DAY03/ex02/FragTrap.hpp
+++ DAY03/ex02/FragTrap.hpp
@@ -17,6 +17,9 @@ class FragTrap : public ClapTrap
     public :
         FragTrap ();
         ~FragTrap ();
+        FragTrap (const FragTrap &other);
+        FragTrap &operator= (const FragTrap &other); 
         FragTrap (std::string name);
         void highFivesGuys();
 };
+
