59183471167654067a56434ae2b7c8e4a3992be0
El Mustapha Zahiri <ezahiri@e3r8p18.1337.ma>
2025-01-23 22:59:33 +0100

    finish ex00 in DAY06

--- DAY06/ex00/ScalarConverter.hpp
+++ DAY06/ex00/ScalarConverter.hpp
@@ -6,10 +6,12 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 15:59:43 by ezahiri           #+#    #+#             */
-/*   Updated: 2025/01/23 22:05:41 by ezahiri          ###   ########.fr       */
+/*   Updated: 2025/01/23 22:55:34 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
 
 #include <iostream>
 #include <sstream>
@@ -27,3 +29,5 @@ class ScalarConverter
         ScalarConverter &operator= (const ScalarConverter &other);
         static void convert  (std::string cv);
 };
+
+#endif
