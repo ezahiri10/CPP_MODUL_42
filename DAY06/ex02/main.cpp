d35b93107e3b947cbbf80492764f5cd788ca5ace
El Mustapha Zahiri <ezahiri@e2r6p7.1337.ma>
2025-01-24 11:54:58 +0100

    DAY06 is Finish

--- DAY06/ex02/main.cpp
+++ DAY06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 23:38:47 by ezahiri           #+#    #+#             */
-/*   Updated: 2025/01/23 23:52:21 by ezahiri          ###   ########.fr       */
+/*   Updated: 2025/01/24 11:53:48 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +36,12 @@ Base * generate(void)
 
 void identify(Base* p)
 {
-    if (dynamic_cast<A *>(p) != nullptr)
-        std::cout << " A " << std::endl;
-    else if (dynamic_cast<B *>(p) != nullptr)
-        std::cout << " B " << std::endl;
-    else if (dynamic_cast<C *>(p) != nullptr)
-        std::cout << " C " << std::endl;
+    if (dynamic_cast<A *>(p))
+        std::cout << " A" << std::endl;
+    else if (dynamic_cast<B *>(p))
+        std::cout << " B" << std::endl;
+    else if (dynamic_cast<C *>(p))
+        std::cout << " C" << std::endl;
     else 
         std::cout << "Unknown type" << std::endl;
 }
@@ -50,30 +50,25 @@ void identify(Base& p)
 {
     try {
         (void)dynamic_cast <A &>(p);
-    }
-    catch (std::exception &e)
-    {
-        (void)e;
         std::cout << " A" << std::endl;
-        return ;
     }
-    try {
-        (void)dynamic_cast <B &>(p);
-    }
-    catch (std::exception &e)
+    catch (std::exception &)
     {
-        (void)e;
-        std::cout << " B" << std::endl;
-        return ;
-    }
-    try {
-        (void)dynamic_cast <C &>(p);
-    }
-    catch (std::exception &e)
-    {
-        (void)e;
-        std::cout << " C" << std::endl;
-        return ;
+        try {
+            (void)dynamic_cast <B &>(p);
+             std::cout << " B" << std::endl;
+        }
+        catch (std::exception &)
+        {
+            try {
+                (void)dynamic_cast <C &>(p);
+                std::cout << " C" << std::endl;
+            }
+            catch (std::exception &)
+            {
+                std::cout << "Unknown type" << std::endl;
+            }
+        }
     }
     
 }
