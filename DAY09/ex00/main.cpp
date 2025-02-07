1c28ff9f3b83d5eaef3809c0647a3a5516288a95
El Mustapha Zahiri <ezahiri@e2r6p7.1337.ma>
2025-02-07 12:16:34 +0100

    fix some issue in ex00 in DAY09

--- DAY09/ex00/main.cpp
+++ DAY09/ex00/main.cpp
@@ -6,19 +6,14 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 21:27:23 by ezahiri           #+#    #+#             */
-/*   Updated: 2025/02/07 12:11:11 by ezahiri          ###   ########.fr       */
+/*   Updated: 2025/02/07 12:16:30 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-void f()
-{
-    system("leaks btc");
-}
 int main (int ac, char **av)
 {
-    atexit(f);
     try {
         if (ac != 2)
             throw std::invalid_argument("Error: could not open file.");
