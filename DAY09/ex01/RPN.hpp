a9a4b5cfa3ba2e2807efa380535569875cf0fe8d
El Mustapha Zahiri <ezahiri@e2r6p7.1337.ma>
2025-02-09 13:04:50 +0100

    add include guards to rpn

--- DAY09/ex01/RPN.hpp
+++ DAY09/ex01/RPN.hpp
@@ -6,9 +6,12 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 16:23:44 by ezahiri           #+#    #+#             */
-/*   Updated: 2025/02/08 23:17:12 by ezahiri          ###   ########.fr       */
+/*   Updated: 2025/02/09 13:03:24 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef RPN_HPP
+#define RPN_HPP
 
 #include <iostream>
 #include <stack>
@@ -24,8 +27,12 @@ class  RPN
         RPN (const RPN &cpy);
         RPN& operator=(const RPN &other);
         void RPNCalculter (const std::string &s);
+        ~RPN();
+    private :
         double StrToDouble(const std::string &s);
         bool IsOperator(int c);
         void Calculater(const std::string &tokens);
-        ~RPN();
+        
 };
+
+#endif
