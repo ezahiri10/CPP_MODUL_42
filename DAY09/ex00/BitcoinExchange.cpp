46fedc4e7ea66dc7bea8e9fce65de0e6b993d3f1
El Mustapha Zahiri <ezahiri@e3r11p6.1337.ma>
2025-02-14 12:18:14 +0100

    finish ex02 in DAY09

--- DAY09/ex00/BitcoinExchange.cpp
+++ DAY09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 20:30:01 by ezahiri           #+#    #+#             */
-/*   Updated: 2025/02/09 12:54:53 by ezahiri          ###   ########.fr       */
+/*   Updated: 2025/02/14 12:15:55 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,22 @@ void BitcoinExchange::exchange()
 {
     std::string line;
     std::ifstream ifile (this->NameFile.c_str());
-    if (ifile.is_open() == false || std::getline(ifile, line).fail())
+
+    if (ifile.is_open() == false)
         throw std::runtime_error("Error: could not open file.");
+    if (std::getline(ifile, line).fail() || line != "date | value")
+    {
+        ifile.close();
+        throw std::invalid_argument("Error: could not open file.");
+    }
     Parser p;
 
     while (std::getline(ifile ,line))
     {
         try 
         {
+            if (line.empty() == true)
+                continue;
             p.CheckForm(line);
             p.ParseDate();
             p.ParseValue();
@@ -49,7 +57,6 @@ void BitcoinExchange::exchange()
         catch(std::exception &e)
         {
             std::cerr << e.what() << std::endl;
-            ifile.close();
         }
     }
     ifile.close();
